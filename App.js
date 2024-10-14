45216f748dc3a20099e92bc7baacd519ec3dfff7
ARSHamirali <arshamirali604@gmail.com>
2024-10-14 18:34:26 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,74 @@
+//                                                             { LOOPS }
+
+//                   (While--loop)
+// var i = 0
+// var arr = [1, 54, "Arsh", undefined, true]
+
+// while (i < arr.length) {
+//     console.log(arr[i])
+//     i++
+// }
+
+
+// var word = prompt("Enter a word")
+// var reversed = ""
+// var i = word.length - 1;
+
+// while (i >= 0) {
+//     reversed += word[i]
+//     i--
+// }
+// if (word === reversed) {
+//     console.log("Word is palindrome")
+// }
+// else {
+//     console.log("Word is not s palindrome")
+// }
+
+
+//                    (Do -while -loop)
+//     [SYNTAX]
+// var i = 0
+// do {
+//     console.log(i)
+//     i++
+// }
+// while (i <= 10)
+
+// MATHS - TABLES
+
+// var num = +prompt("Enter a number")
+// var i = 1
+// do {
+//     console.log(num, "x", i, "=", (i * num))
+//     i++
+// }
+// while (i <= 10)
+
+
+//                 (EVENTS {CONNECTING HTML AND JS })
+// function greeting() {
+//     alert("Sing Up successfully !!")
+// }
+
+// function Signup() {
+//     Swal.fire({
+//         title: "Good job!",
+//         text: "Sign Up Successfilly!",
+//         icon: "success"
+//     });
+// }
+
+// function greeting(element) {
+//     console.log(element)
+//     element.style.color = "red"
+//     element.style.backgroundColor = "black"
+
+// }
+
+// function greetingstop(element1) {
+
+//     element1.style.color = "black"
+//     element1.style.backgroundColor = "white"
+
+// }
